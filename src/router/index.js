75c999f8a8a36fc33e0de3e67655d4a7979cd094
EsraBenX <92519523+EsraBenX@users.noreply.github.com>
2022-10-14 12:42:49 +0200

    klein dingetje

--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,29 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import ProgrammerProfilePage from "@/views/ProgrammerProfilePage";
+import ProgrammerHomePage from "@/views/ProgrammerHomePage";
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-    {path: '/programmerProfile' , name:  'ProgrammerProfilePage', component: ProgrammerProfilePage}
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {path: '/programmerProfile', name: 'ProgrammerProfilePage', component: ProgrammerProfilePage},
+    {
+        path: '/ProgrammerHomePage',
+        name: 'ProgrammerHomePage',
+        component: ProgrammerHomePage
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  // eslint-disable-next-line no-undef
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    // eslint-disable-next-line no-undef
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/ProgrammerHomePage.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: "ProgrammerHomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
