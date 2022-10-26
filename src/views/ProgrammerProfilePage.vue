3137411e8dc354101dd3cd3abf10ab9c3a99946f
Vaak09 <91531712+Vaak09@users.noreply.github.com>
2022-10-26 10:15:07 +0200

    revertchanges back

--- src/views/ProgrammerProfilePage.vue
+++ src/views/ProgrammerProfilePage.vue
@@ -304,7 +304,7 @@
             <div class="d-flex align-items-center w-200px w-sm-300px flex-column mt-3">
               <div class="d-flex justify-content-between w-100 mt-auto mb-2">
                 <span class="fw-bold fs-6 text-gray-400">Hours still available</span>
-                <span class="fw-bolder fs-6">20 hours</span>
+                <span class="fw-bolder fs-6">6 hours</span>
               </div>
               <div class="h-5px mx-3 w-100 bg-light mb-3">
                 <div class="bg-success rounded h-5px" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
