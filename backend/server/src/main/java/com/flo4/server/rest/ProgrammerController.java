d646018493ea7a74ff491c2f2bf66dc4094d14f5
Vaak09 <valentijn.nijhuis@hva.nl>
2022-11-16 16:58:21 +0100

    controller

--- backend/server/src/main/java/com/flo4/server/rest/ProgrammerController.java
+++ backend/server/src/main/java/com/flo4/server/rest/ProgrammerController.java
@@ -1,9 +1,66 @@
 package com.flo4.server.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.flo4.server.Exceptions.NotFoundException;
+import com.flo4.server.models.Programmer;
+import com.flo4.server.models.Project;
+import com.flo4.server.repository.EntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RestController
 @RequestMapping("programmers")
 public class ProgrammerController {
+
+    @Autowired
+    EntityRepository<Programmer> programmerRepository;
+
+    @GetMapping(path = "", produces = "application/json")
+    public List<Programmer> getAllProgrammers(){
+        return this.programmerRepository.findAll();
+    }
+
+    @GetMapping(path = "{id}", produces = "application/json")
+    public ResponseEntity<Programmer> findOneProgrammer(@PathVariable() int id){
+        Programmer programmer = this.programmerRepository.findById(id);
+        if (programmer == null){
+            throw new NotFoundException(String.format("Project with id %d was not found!", id));
+        }
+
+        return ResponseEntity.ok().body(programmer);
+    }
+    @Transactional
+    @PutMapping(path = "add", produces = "application/json")
+    public ResponseEntity<Programmer> addProgrammer( @RequestBody Programmer programmer){
+
+        Programmer newProgrammer = this.programmerRepository.save(programmer);
+
+
+        return ResponseEntity.ok().body(newProgrammer);
+    }
+    @DeleteMapping(path = "{id}",produces = "application/json")
+    public ResponseEntity<Programmer> deleteProgrammer(@PathVariable int id){
+        Programmer programmer = this.programmerRepository.findById(id);
+
+        if (programmer == null) {
+            throw new NotFoundException(String.format("Project with id %d was not found!", id));
+        }
+        this.programmerRepository.deleteById(id);
+
+        return ResponseEntity.ok().body(programmer);
+    }
+
+    @PutMapping(path = "{id}", produces = "application/json")
+    public ResponseEntity<Programmer> updateProgrammer(@PathVariable int id, @RequestBody Programmer programmer){
+        Programmer updateProgrammer = this.programmerRepository.update(programmer,id);
+
+        if (updateProgrammer == null){
+            throw new NotFoundException(String.format("Project with id %d was not found!", id));
+        }
+
+        return ResponseEntity.ok().body(updateProgrammer);
+    }
 }
