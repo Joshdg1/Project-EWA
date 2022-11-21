eaf2d8c1760857e97dc76df6f62ba293f22898c5
Joshdg1 <joshdg1999@gmail.com>
2022-11-21 19:19:24 +0100

    temporary filter

--- src/views/admin/client/CreateClientProject-2.vue
+++ src/views/admin/client/CreateClientProject-2.vue
@@ -1,66 +1,66 @@
 <template>
+  <div>
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card card-xl-stretch mb-5 mb-xl-8">
+          <!--begin::Header-->
+          <div class="card-header border-0 pt-5">
+            <h3 class="card-title align-items-start flex-column">
+              <span class="card-label fw-bolder fs-3 mb-1">Add programmers to the project</span>
+            </h3>
+          </div>
+          <!--end::Header-->
+          <!--begin::Body-->
+          <div class="card-body py-3">
+            <!--begin::Table container-->
+            <div class="table-responsive">
+              <!--begin::Table-->
+              <table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
+                <!--begin::Table head-->
+                <thead>
+                <tr class="fw-bolder text-muted">
+                  <th class="w-25px">
+                    
+                  </th>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>Address</th>
+                  <th>City</th>
+                  <th>Postcode</th>
+                  <th>Knowledge</th>
+                  <th>Experience</th>
+                  <th>Availability</th>
+                  <th>Weekly work hours</th>
+                  <th>Work space</th>
+                  <th>Project types</th>
+                  <th>Actions</th>
+                </tr>
+                </thead>
+                <!--end::Table head-->
+                <!--begin::Table body-->
+                <tbody>
+                <tr v-for="programmer in programmers" v-bind:key="programmer">
+                  <div class="form-check form-check-sm form-check-custom form-check-solid">
+                    <input class="form-check-input" type="checkbox" value="1" data-kt-check="true"
+                           data-kt-check-target=".widget-9-check"/>
+                  </div>
+                  <td>{{ programmer.firstname }}</td>
+                  <td>{{ programmer.lastname }}</td>
+                  <td>{{ programmer.address }}</td>
+                  <td>{{ programmer.city }}</td>
+                  <td>{{ programmer.postcode }}</td>
+                  <td>{{ programmer.knowledge }}</td>
+                  <td>{{ programmer.experience }}</td>
+                  <td>{{ programmer.availability }}</td>
+                  <td>{{ programmer.hours }}</td>
+                  <td>{{ programmer.workPreference }}</td>
+                  <td>{{ programmer.projectType }}</td>
 
 
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="card card-xl-stretch mb-5 mb-xl-8">
-        <!--begin::Header-->
-        <div class="card-header border-0 pt-5">
-          <h3 class="card-title align-items-start flex-column">
-            <span class="card-label fw-bolder fs-3 mb-1">Add programmers to the project</span>
-          </h3>
-        </div>
-        <!--end::Header-->
-        <!--begin::Body-->
-        <div class="card-body py-3">
-          <!--begin::Table container-->
-          <div class="table-responsive">
-            <!--begin::Table-->
-            <table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4">
-              <!--begin::Table head-->
-              <thead>
-              <tr class="fw-bolder text-muted">
-                <th class="w-25px">
-                </th>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Address</th>
-                <th>City</th>
-                <th>Postcode</th>
-                <th>Knowledge</th>
-                <th>Experience</th>
-                <th>Availability</th>
-                <th>Weekly work hours</th>
-                <th>Work space</th>
-                <th>Project types</th>
-                <th>Actions</th>
-              </tr>
-              </thead>
-              <!--end::Table head-->
-              <!--begin::Table body-->
-              <tbody>
-              <tr v-for="programmer in programmers" v-bind:key="programmer">
-                <div class="form-check form-check-sm form-check-custom form-check-solid">
-                  <input class="form-check-input" type="checkbox" value="1" data-kt-check="true"
-                         data-kt-check-target=".widget-9-check"/>
-                </div>
-                <td>{{ programmer.firstname }}</td>
-                <td>{{ programmer.lastname }}</td>
-                <td>{{ programmer.address }}</td>
-                <td>{{ programmer.city }}</td>
-                <td>{{ programmer.postcode }}</td>
-                <td>{{ programmer.knowledge }}</td>
-                <td>{{ programmer.experience }}</td>
-                <td>{{ programmer.availability }}</td>
-                <td>{{ programmer.hours }}</td>
-                <td>{{ programmer.workPreference }}</td>
-                <td>{{ programmer.projectType }}</td>
-
-
-                <div class="d-flex justify-content-end flex-shrink-0">
-                  <a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
-                    <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
-                    <span class="svg-icon svg-icon-3">
+                  <div class="d-flex justify-content-end flex-shrink-0">
+                    <a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm me-1">
+                      <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
+                      <span class="svg-icon svg-icon-3">
 																			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                            fill="none">
 																				<path opacity="0.3"
@@ -71,11 +71,11 @@
                                             fill="black"/>
 																			</svg>
 																		</span>
-                    <!--end::Svg Icon-->
-                  </a>
-                  <a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
-                    <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
-                    <span class="svg-icon svg-icon-3">
+                      <!--end::Svg Icon-->
+                    </a>
+                    <a href="#" class="btn btn-icon btn-bg-light btn-active-color-primary btn-sm">
+                      <!--begin::Svg Icon | path: icons/duotune/general/gen027.svg-->
+                      <span class="svg-icon svg-icon-3">
 																			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                            fill="none">
 																				<path
@@ -89,65 +89,77 @@
                                               fill="black"/>
 																			</svg>
 																		</span>
-                  </a>
-                </div>
+                    </a>
+                  </div>
 
-              </tr>
-              </tbody>
-              <!--end::Table body-->
-            </table>
-            <!--end::Table-->
+                </tr>
+                </tbody>
+                <!--end::Table body-->
+              </table>
+              <!--end::Table-->
+            </div>
+            <!--end::Table container-->
           </div>
-          <!--end::Table container-->
+          <!--begin::Body-->
         </div>
-        <!--begin::Body-->
+        <!--end::Tables Widget 3-->
       </div>
-      <!--end::Tables Widget 3-->
-    </div>
-    <div class="d-flex flex-shrink-0">
-      <div class="d-flex ms-3">
-        <router-link to="/createNewProject" class="btn background-florijn btn-active-info">
-          Back
-        </router-link>
-      </div>
-      <div class="d-flex ms-3">
-        <router-link to="/" class="btn background-florijn btn-active-info">
-          Submit
-        </router-link>
+      <div class="d-flex flex-shrink-0">
+        <div class="d-flex ms-3">
+          <router-link to="/createNewProject" class="btn background-florijn btn-active-info">
+            Back
+          </router-link>
+        </div>
+        <div class="d-flex ms-3">
+          <router-link to="/" class="btn background-florijn btn-active-info">
+            Submit
+          </router-link>
+        </div>
+
       </div>
 
     </div>
-
   </div>
-
 </template>
 
 <script>
 import Programmer from '../../../models/programmer/programmer.js'
-import Multiselect from 'vue-multi-select'
 
 export default {
   name: "CreateClientProject-2",
-  components:{
-    Multiselect
-  },
 
   created() {
     for (let i = 0; i < 8; i++) {
       this.programmers.push(Programmer.createProgrammer());
+      this.skillWithoutFilter.push(Programmer.createProgrammer());
     }
   },
 
   data() {
     return {
-      value:[],
       programmers: [],
+      programmerNameFilter:"",
+      skillWithoutFilter: []
+
     }
   },
+  methods:{
+    FilterFn(){
+      let skillNameFilter = this.skillNameFilter
 
-  computed: {
+      this.programmers = this.skillWithoutFilter.filter(
+          function (el){
+            return el.postcode.toString().toLowerCase().includes(
+                skillNameFilter.toString().trim().toLowerCase()
+            )
+          }
+      )
 
-  }
+    },
+
+  },
+
+  computed: {}
 }
 </script>
 
