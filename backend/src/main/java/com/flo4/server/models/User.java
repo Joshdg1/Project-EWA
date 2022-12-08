b4a8601a10ffcbb2b2dec8f22ec8db3c8827b47c
Vaak09 <91531712+Vaak09@users.noreply.github.com>
2022-12-08 22:57:12 +0100

    user

---
+++ backend/src/main/java/com/flo4/server/models/User.java
@@ -0,0 +1,50 @@
+package com.flo4.server.models;
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String phoneNumber;
+    private String userType;
+
+    public User(){}
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public static User of(int id, String email, String firstName, String lastName, String password, String phoneNumber) {
+        return new User(id, email,firstName, lastName, password, phoneNumber, null);
+    }
+
+
+    public User(int id, String email, String firstName, String lastName, String password, String phoneNumber, String userType) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.userType = userType;
+    }
+
+
+}
---
+++ backend/src/main/java/com/flo4/server/repository/UserRepository.java
@@ -0,0 +1,83 @@
+package com.flo4.server.repository;
+
+
+import com.flo4.server.models.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Repository("USER.JPA")
+@Transactional
+public class UserRepository implements EntityRepository<User> {
+
+    @PersistenceContext
+    protected EntityManager entityManager;
+
+    @Override
+    public List<User> findAll() {
+        TypedQuery<User> query = this.entityManager.createQuery("select u from User u", User.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public User findById(int id) {
+        return this.entityManager.find(User.class, id);
+    }
+
+    @Override
+    public User save(User entity) {
+        return this.entityManager.merge(entity);
+    }
+
+    @Override
+    public User update(User entity, int id) {
+        User updatedUser = findById(id);
+
+        if (updatedUser == null) {
+            return null;
+        }
+
+        updatedUser.setEmail(entity.getEmail());
+        updatedUser.setFirstName(entity.getFirstName());
+        updatedUser.setLastName(entity.getLastName());
+        updatedUser.setPassword(entity.getPassword());
+        updatedUser.setPhoneNumber(entity.getPhoneNumber());
+
+
+        return updatedUser;
+    }
+
+    @Override
+    public User deleteById(int id) {
+        User user = entityManager.find(User.class, id);
+        entityManager.remove(user);
+        return user;
+
+    }
+
+    @Override
+    public List<User> findByQuery(String jpqlName, Object... params) {
+        TypedQuery<User> query = this.entityManager.createNamedQuery(jpqlName, User.class);
+
+        //Put all params into the query.
+        for (int i = 0; i < params.length; i++) {
+            query.setParameter(i + 1, params[i]);
+        }
+
+        //Execute the query and return all the results.
+        return query.getResultList();
+    }
+
+    //of de user returned
+    //of returned null
+    public User findByEmail(String email) {
+        return this.entityManager.createQuery("select u from User u where email = ?1", User.class).setParameter(1, email).getSingleResult();
+    }
+
+}
