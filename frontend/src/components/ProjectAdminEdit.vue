139ed6918c842c8831b6f06a7d95e03f57005610
Kick Ooteman <kickooteman@gmail.com>
2022-12-18 14:44:00 +0100

    Fixed edit and delete for programmer

--- frontend/src/components/ProjectAdminEdit.vue
+++ frontend/src/components/ProjectAdminEdit.vue
@@ -9,18 +9,6 @@
           </h3>
           <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover"
                title="Click to add a project">
-            <a href="#" class="btn btn-sm btn-light-primary" data-bs-toggle="modal"
-               data-bs-target="#kt_modal_invite_friends">
-              <!--begin::Svg Icon | path: icons/duotune/arrows/arr075.svg-->
-              <span class="svg-icon svg-icon-3">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                               fill="none">
-														<rect opacity="0.5" x="11.364" y="20.364" width="16" height="2" rx="1"
-                                  transform="rotate(-90 11.364 20.364)" fill="black"/>
-														<rect x="4.36396" y="11.364" width="16" height="2" rx="1" fill="black"/>
-													</svg>
-												</span>
-              <!--end::Svg Icon-->New Project</a>
           </div>
         </div>
         <!--end::Header-->
