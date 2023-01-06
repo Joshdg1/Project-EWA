59fafb5523ccce0535b75966e1974a229f62789c
EsraBen <esra.jemni@hva.nl>
2023-01-06 13:12:27 +0100

    d

--- backend/src/main/java/com/flo4/server/rest/UserController.java
+++ backend/src/main/java/com/flo4/server/rest/UserController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(value = "users")
+@RequestMapping(value = "/users")
 public class UserController {
     private final UserService userService;
 
