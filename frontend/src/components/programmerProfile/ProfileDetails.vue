12fcfc8d82f824e7d9914443da7117bb0fe4c726
Joshdg1 <joshdg1999@gmail.com>
2023-01-10 20:00:18 +0100

    change to fe

--- frontend/src/components/programmerProfile/ProfileDetails.vue
+++ frontend/src/components/programmerProfile/ProfileDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+  <div  class="card mb-5 mb-xl-10" id="kt_profile_details_view">
     <!--begin::Card header-->
     <div class="card-header cursor-pointer">
       <!--begin::Card title-->
@@ -10,81 +10,63 @@
       <!--begin::Action-->
       <div class="d-flex ms-3">
         <div style="margin-top: 20px">
-          <button @click="setEditProfile()" class="mb-2 btn btn-sm bg-primary btn-active-info" tooltip="New App"
-                  data-bs-toggle="modal"
-                  data-bs-target="#kt_modal_create_app" id="kt_toolbar_primary_button">Wijzig profiel-pagina
-          </button>
+          <a @click="setEditProfile()" class="mb-2 btn btn-sm bg-primary btn-active-info" tooltip="New App" data-bs-toggle="modal"
+             data-bs-target="#kt_modal_create_app" id="kt_toolbar_primary_button">Wijzig profiel-pagina</a>
         </div>
-
-        <!--        <form>-->
-        <!--      <input ref="file" type="file" @change="onFileSelected()">-->
-        <!--        </form>-->
-
-        <!--        <div style="margin-top: 20px">-->
-        <!--          <button @click="onUpload()" class="mb-2 btn btn-sm bg-primary btn-active-info" tooltip="New App" data-bs-toggle="modal"-->
-        <!--             data-bs-target="#kt_modal_create_app" type="submit">upload cv</button>-->
-        <!--        </div>-->
-        <!--      </div>-->
-
-        <form ref="uploadForm" @submit.prevent="submit">
-          <input type="file" ref="uploadFile" @change="onFileUpload()" class="form-control" required>
-          <input type="button" @click="startUpload()" name="Upload" value="Upload">
-        </form>
-
-        <!--end::Action-->
       </div>
-      <!--begin::Card header-->
-      <!--begin::Card body-->
-      <div class="card-body p-9">
-        <!--begin::Row-->
-        <div class="row mb-7">
-          <!--begin::Label-->
-          <label class="col-lg-4 fw-bold text-muted">Voornaam(en)</label>
-          <!--end::Label-->
-          <!--begin::Col-->
-          <div class="col-lg-8 fv-row">
-            <span class="fw-bolder fs-6 text-gray-800">{{ sampleProgrammer.firstName }} </span>
-          </div>
-          <!--end::Col-->
-        </div>
-        <div class="row mb-7">
-          <!--begin::Label-->
-          <label class="col-lg-4 fw-bold text-muted">Achternaam</label>
-          <!--end::Label-->
-          <!--begin::Col-->
-          <div class="col-lg-8 fv-row">
-            <span class="fw-bold fs-6 text-gray-800">{{ sampleProgrammer.lastName }}</span>
-          </div>
-          <!--end::Col-->
-        </div>
-        <!--end::Row-->
-        <!--begin::Input group-->
-        <div class="row mb-7">
-          <!--begin::Label-->
-          <label class="col-lg-4 fw-bold text-muted">Email-adres</label>
-          <!--end::Label-->
-          <!--begin::Col-->
-          <div class="col-lg-8 fv-row">
-            <span class="fw-bold fs-6 text-gray-800">{{ sampleProgrammer.email }}</span>
-          </div>
-          <!--end::Col-->
-        </div>
-        <div class="row mb-7">
-          <!--begin::Label-->
-          <label class="col-lg-4 fw-bold text-muted">Telefoonnummer</label>
-          <!--end::Label-->
-          <!--begin::Col-->
-          <div class="col-lg-8 fv-row">
-            <span class="fw-bold fs-6 text-gray-800">{{ sampleProgrammer.phoneNumber }}</span>
-          </div>
-          <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-        <!--begin::Input group-->
-
-      </div>
-      <!--end::Card body-->
+      <!--end::Action-->
     </div>
+    <!--begin::Card header-->
+    <!--begin::Card body-->
+    <div class="card-body p-9">
+      <!--begin::Row-->
+      <div class="row mb-7">
+        <!--begin::Label-->
+        <label class="col-lg-4 fw-bold text-muted">Voornaam(en)</label>
+        <!--end::Label-->
+        <!--begin::Col-->
+        <div class="col-lg-8 fv-row">
+          <span class="fw-bolder fs-6 text-gray-800">{{sampleProgrammer.firstName}} </span>
+        </div>
+        <!--end::Col-->
+      </div>
+      <div class="row mb-7">
+        <!--begin::Label-->
+        <label class="col-lg-4 fw-bold text-muted">Achternaam</label>
+        <!--end::Label-->
+        <!--begin::Col-->
+        <div class="col-lg-8 fv-row">
+          <span class="fw-bold fs-6 text-gray-800">{{sampleProgrammer.lastName}}</span>
+        </div>
+        <!--end::Col-->
+      </div>
+      <!--end::Row-->
+      <!--begin::Input group-->
+      <div class="row mb-7">
+        <!--begin::Label-->
+        <label class="col-lg-4 fw-bold text-muted">Email-adres</label>
+        <!--end::Label-->
+        <!--begin::Col-->
+        <div class="col-lg-8 fv-row">
+          <span class="fw-bold fs-6 text-gray-800">{{ sampleProgrammer.email }}</span>
+        </div>
+        <!--end::Col-->
+      </div>
+      <div class="row mb-7">
+        <!--begin::Label-->
+        <label class="col-lg-4 fw-bold text-muted">Telefoonnummer</label>
+        <!--end::Label-->
+        <!--begin::Col-->
+        <div class="col-lg-8 fv-row">
+          <span class="fw-bold fs-6 text-gray-800">{{ sampleProgrammer.phoneNumber }}</span>
+        </div>
+        <!--end::Col-->
+      </div>
+      <!--end::Input group-->
+      <!--begin::Input group-->
+
+    </div>
+    <!--end::Card body-->
   </div>
 </template>
 
