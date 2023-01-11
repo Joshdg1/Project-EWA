e92736e1ba3f3f76ac55e101bb0887cb286b7897
Kick Ooteman <kickooteman@gmail.com>
2023-01-11 02:59:28 +0100

    Resetpassword live

--- backend/src/main/java/com/flo4/server/rest/UserController.java
+++ backend/src/main/java/com/flo4/server/rest/UserController.java
@@ -179,7 +179,7 @@ public class UserController {
             passwordResetTokens.setToken(tokenRegisterReset);
             passwordResetTokens.setUser_id(user);
 
-            String resetPasswordLink = "http://localhost:8080/#/users/resetPassword?token=" + tokenRegisterReset;
+            String resetPasswordLink = "https://ewa-be-app-production-6e5c.up.railway.app/#/users/resetPassword?token=" + tokenRegisterReset;
 
             passwordResetRepository.save(passwordResetTokens);
 
@@ -363,7 +363,7 @@ public class UserController {
         passwordResetTokens.setUser_id(user);
 
 
-        String resetPasswordLink = "http://localhost:8080/#/users/resetPassword?token=" + token;
+        String resetPasswordLink = "https://ewa-be-app-production-6e5c.up.railway.app/#/users/resetPassword?token=" + token;
 
         passwordResetRepository.save(passwordResetTokens);
 
